c92ec128a655567844f205e53da64210123085c3
Omid Noorshams <o.noorshams@gmail.com>
2023-05-14 19:53:05 -0700

    optimize shadow map camera variables

--- chapter-02--classic-techniques/16-shadows/src/script.js
+++ chapter-02--classic-techniques/16-shadows/src/script.js
@@ -35,6 +35,19 @@ directionalLight.castShadow = true;
 
 directionalLight.shadow.mapSize.width = 1024;
 directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.camera.near = 2;
+directionalLight.shadow.camera.far = 6;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.bottom = -2;
+directionalLight.shadow.radius = 4;
+
+const directionalLightCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera,
+);
+directionalLightCameraHelper.visible = false;
+scene.add(directionalLightCameraHelper);
 
 /**
  * Materials
