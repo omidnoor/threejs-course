dae8f90e38e58c0dfebb1dccda0efe327685107f
Omid Noorshams <o.noorshams@gmail.com>
2023-05-14 22:19:08 -0700

    add graves

--- chapter-02--classic-techniques/17-haunted-house/src/script.js
+++ chapter-02--classic-techniques/17-haunted-house/src/script.js
@@ -24,6 +24,7 @@ const textureLoader = new THREE.TextureLoader();
  */
 const house = new THREE.Group();
 scene.add(house);
+house.castShadow = true;
 
 // Walls
 const walls = new THREE.Mesh(
@@ -42,6 +43,63 @@ roof.position.y = 2.5 + 0.5;
 roof.rotation.y = -Math.PI * 0.25;
 house.add(roof);
 
+// Door
+const door = new THREE.Mesh(
+  new THREE.PlaneGeometry(2, 2),
+  new THREE.MeshStandardMaterial({ color: "#aa7b7b" }),
+);
+door.position.z = 2 + 0.01;
+door.position.y = 1;
+door.rotation.z = Math.PI * 0.5;
+house.add(door);
+
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshBasicMaterial({
+  color: "#89c854",
+});
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
+
+// Graves
+const graves = new THREE.Group();
+house.add(graves);
+
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({
+  color: "#b2b6b1",
+});
+
+for (let i = 0; i < 30; i++) {
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3 + Math.random() * 6;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+  grave.position.set(x, 0.4, z);
+  graves.add(grave);
+}
+
+const grave1 = new THREE.Mesh(graveGeometry, graveMaterial);
+
+graves.add(grave1);
+
 // Floor
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
@@ -50,6 +108,7 @@ const floor = new THREE.Mesh(
 floor.rotation.x = -Math.PI * 0.5;
 floor.position.y = 0;
 scene.add(floor);
+floor.receiveShadow = true;
 
 /**
  * Lights
@@ -58,6 +117,7 @@ scene.add(floor);
 const ambientLight = new THREE.AmbientLight("#ffffff", 0.5);
 gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
 scene.add(ambientLight);
+ambientLight.castShadow = true;
 
 // Directional light
 const moonLight = new THREE.DirectionalLight("#ffffff", 0.5);
@@ -67,6 +127,7 @@ gui.add(moonLight.position, "x").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "y").min(-5).max(5).step(0.001);
 gui.add(moonLight.position, "z").min(-5).max(5).step(0.001);
 scene.add(moonLight);
+moonLight.castShadow = true;
 
 /**
  * Sizes
@@ -117,6 +178,8 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+renderer.shadowMap.enabled = true;
 
 /**
  * Animate
