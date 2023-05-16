04bab39ccd6cc6ce9a3209fd9ac1f861d82611e1
Omid Noorshams <o.noorshams@gmail.com>
2023-05-15 23:04:25 -0700

    normalize parallax effect based on screen frequency

--- chapter-02--classic-techniques/20-scroll-based-animation/src/script.js
+++ chapter-02--classic-techniques/20-scroll-based-animation/src/script.js
@@ -143,16 +143,22 @@ window.addEventListener("mousemove", (event) => {
  */
 const clock = new THREE.Clock();
 
+let previousTime = 0;
+
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
   // Animate Camera
   camera.position.y = (-scrollY / sizes.height) * objectsDistance;
 
-  const parallaxX = cursor.x;
-  const parallaxY = -cursor.y;
-  cameraGroup.position.x = parallaxX;
-  cameraGroup.position.y = parallaxY;
+  const parallaxX = cursor.x * 0.5;
+  const parallaxY = -cursor.y * 0.5;
+  cameraGroup.position.x +=
+    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+  cameraGroup.position.y +=
+    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
 
   // Animate Meshes
   for (const mesh of sectionMeshes) {
