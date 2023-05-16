71573a2841658f2d1e7a4107d6ddb74ad7ac4eb9
Omid Noorshams <o.noorshams@gmail.com>
2023-05-15 17:31:57 -0700

    add branches, andomness, ...

--- chapter-02--classic-techniques/19-galaxy-generator/src/script.js
+++ chapter-02--classic-techniques/19-galaxy-generator/src/script.js
@@ -20,6 +20,11 @@ const scene = new THREE.Scene();
 const parameters = {};
 parameters.count = 1000;
 parameters.size = 0.02;
+parameters.radius = 5;
+parameters.branches = 3;
+parameters.spin = 1;
+parameters.randomness = 0.2;
+parameters.randomnessPower = 3;
 
 let geometry = null;
 let material = null;
@@ -35,13 +40,36 @@ const generateGalaxy = () => {
   /**
    *  Geometry
    */
+  let z = 0;
   geometry = new THREE.BufferGeometry();
   const positions = new Float32Array(parameters.count * 3);
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
-    positions[i3] = (Math.random() - 0.5) * 3;
-    positions[i3 + 1] = (Math.random() - 0.5) * 3;
-    positions[i3 + 2] = (Math.random() - 0.5) * 3;
+    const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? -1 : 1) *
+      parameters.randomness;
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? -1 : 1) *
+      parameters.randomness;
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) *
+      (Math.random() < 0.5 ? -1 : 1) *
+      parameters.randomness;
+
+    positions[i3] =
+      Math.cos(branchAngle + spinAngle) * radius +
+      randomX * (radius / parameters.radius) ** 2;
+    positions[i3 + 1] = Math.sin(randomY) ** 11;
+    positions[i3 + 2] =
+      Math.sin(branchAngle + spinAngle) * radius +
+      randomZ * (radius / parameters.radius) ** 2;
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 
@@ -74,6 +102,41 @@ gui
   .add(parameters, "size")
   .min(0.001)
   .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "spin")
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "randomness")
+  .min(1)
+  .max(2)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "randomnessPower")
+  .min(1)
+  .max(10)
   .step(0.001)
   .onFinishChange(generateGalaxy);
 
