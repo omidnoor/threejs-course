eabdee44c83a314c21eaa5af2f0d7612a01809ac
Omid Noorshams <o.noorshams@gmail.com>
2023-05-16 02:58:41 -0700

    add material to bodies

--- chapter-03--advanced-techniques/21-physics/src/script.js
+++ chapter-03--advanced-techniques/21-physics/src/script.js
@@ -40,13 +40,26 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -10, 0);
 
+// Materials
+const defaultMaterial = new CANNON.Material("default");
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.9,
+  },
+);
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
+
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
-  material: new CANNON.Material(),
 });
 world.addBody(sphereBody);
 
