a1c2a77ffbeab81f1a4d6d3cfa07fe79f53afd13
Omid Noorshams <o.noorshams@gmail.com>
2023-05-16 22:22:18 -0700

    animate objects. cast a ray through objects. change color if ray passes through object

--- chapter-03--advanced-techniques/23-raycaster-and-mouse-events/src/script.js
+++ chapter-03--advanced-techniques/23-raycaster-and-mouse-events/src/script.js
@@ -40,16 +40,18 @@ scene.add(object1, object2, object3);
  * Raycaster
  */
 const raycaster = new THREE.Raycaster();
-const rayOrigin = new THREE.Vector3(-3, 0, 0);
-const rayDirection = new THREE.Vector3(1, 0, 0);
-rayDirection.normalize();
-raycaster.set(rayOrigin, rayDirection);
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const rayDirection = new THREE.Vector3(1, 0, 0);
+// rayDirection.normalize();
+// raycaster.set(rayOrigin, rayDirection);
 // console.log(raycaster);
-const intersect = raycaster.intersectObject(object2);
+// const intersect = raycaster.intersectObject(object2);
 // console.log(intersect);
 
-const intersects = raycaster.intersectObjects(scene.children);
-console.log(intersects);
+// const intersects = raycaster.intersectObjects(scene.children);
+// console.log(intersects);
+
 /**
  * Sizes
  */
@@ -105,6 +107,28 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animate Objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.5) * 1.5;
+
+  // Cast a Ray
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0);
+  rayDirection.normalize();
+  raycaster.set(rayOrigin, rayDirection);
+
+  const objectToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(scene.children);
+
+  for (const object of objectToTest) {
+    object.material.color.set("#ff0000");
+  }
+
+  for (const intersect of intersects) {
+    intersect.object.material.color.set("#0000ff");
+  }
 
   // Update controls
   controls.update();
