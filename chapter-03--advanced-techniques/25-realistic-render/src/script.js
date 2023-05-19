38817ef699614c57c49d5875eefad3772ab5e5fe
Omid Noorshams <o.noorshams@gmail.com>
2023-05-18 18:56:46 -0700

    add envMap to model materials traverse. add envMap intensity to gui

--- chapter-03--advanced-techniques/25-realistic-render/src/script.js
+++ chapter-03--advanced-techniques/25-realistic-render/src/script.js
@@ -7,12 +7,14 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
  * Loaders
  */
 const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI();
+const debugObject = {};
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -21,13 +23,41 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Test sphere
+ * Update all materials
  */
-// const testSphere = new THREE.Mesh(
-//   new THREE.SphereGeometry(1, 32, 32),
-//   new THREE.MeshStandardMaterial(),
-// );
-// scene.add(testSphere);
+const updateMaterials = () => {
+  scene.traverse((child) => {
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      child.material.envMap = environmentMap;
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+    }
+  });
+};
+
+/**
+ * Enviroment map
+ */
+const environmentMap = cubeTextureLoader.load([
+  "/textures/environmentMaps/0/px.jpg",
+  "/textures/environmentMaps/0/nx.jpg",
+  "/textures/environmentMaps/0/py.jpg",
+  "/textures/environmentMaps/0/ny.jpg",
+  "/textures/environmentMaps/0/pz.jpg",
+  "/textures/environmentMaps/0/nz.jpg",
+]);
+scene.background = environmentMap;
+
+debugObject.envMapIntensity = 5;
+gui
+  .add(debugObject, "envMapIntensity")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("envMap")
+  .onChange(updateMaterials);
 
 /**
  * Models
@@ -44,6 +74,8 @@ gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
     .max(Math.PI)
     .step(0.001)
     .name("rotation");
+
+  updateMaterials();
 });
 
 /**
