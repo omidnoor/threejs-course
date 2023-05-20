2e75340c2e142218347f9e59b23c48f759bbd8a0
Omid Noorshams <o.noorshams@gmail.com>
2023-05-20 16:30:48 -0700

    add methods startLoading and setLoaders to Resources class

--- chapter-03--advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
+++ chapter-03--advanced-techniques/26-code-structuring-for-bigger-projects/src/Experience/Utils/Resources.js
@@ -16,12 +16,32 @@ export default class Resources extends EventEmitter {
     this.loaded = 0;
 
     this.setLoaders();
+    this.startLoading();
   }
 
   setLoaders() {
     this.loaders = {};
     this.loaders.gltfLoader = new GLTFLoader();
     this.loaders.textureLoader = new THREE.TextureLoader();
-    this.loaders.cubeLoader = new THREE.CubeTextureLoader();
+    this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader();
+  }
+
+  startLoading() {
+    // Load each source
+    for (const source of this.sources) {
+      if (source.type === "gltfModel") {
+        this.loaders.gltfLoader.load(source.path, (file) => {
+          console.log(file);
+        });
+      } else if (source.type === "texture") {
+        this.loaders.textureLoader.load(source.path, (file) => {
+          console.log(file);
+        });
+      } else if (source.type === "cubeTexture") {
+        this.loaders.cubeTextureLoader.load(source.path, (file) => {
+          console.log(file);
+        });
+      }
+    }
   }
 }
