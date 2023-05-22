f7090075142fe3ed502c8fe4ad2f81140c4ce4d7
Omid Noorshams <o.noorshams@gmail.com>
2023-05-22 04:14:14 -0700

    add random attribute to geometry. get random in glsl

--- chapter-03--advanced-techniques/27-shaders/src/shaders/test/fragment.glsl
+++ chapter-03--advanced-techniques/27-shaders/src/shaders/test/fragment.glsl
@@ -1,5 +1,7 @@
 precision mediump float;
 uniform float time;
+varying float vRandom;
 void main(){
-    gl_FragColor = vec4(0.7, 0.2 * sin(time), 0.5, 1.0);
+    // gl_FragColor = vec4(vRandom * vRandom, vRandom *vRandom *vRandom, vRandom, sin(vRandom));
+    gl_FragColor = vec4(1.0,0,vRandom,1.0);
 }
--- chapter-03--advanced-techniques/27-shaders/src/shaders/test/vertex.glsl
+++ chapter-03--advanced-techniques/27-shaders/src/shaders/test/vertex.glsl
@@ -1,13 +1,21 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+
+attribute float aRandom;
 attribute vec3 position;
+
+varying float vRandom;
+
 void main(){
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z +=  sin(modelPosition.x * 20.0)*0.1;
+    // modelPosition.z +=  sin(modelPosition.x * 20.0)*0.1;
+    // modelPosition.z +=  sin(modelPosition.x * aRandom* 20.0)*0.1;
 
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
+
+    vRandom = aRandom;
 }
