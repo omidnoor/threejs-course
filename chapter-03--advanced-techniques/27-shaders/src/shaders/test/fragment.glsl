327f5bcd8896eb213c4e7eae224d77e8ac245599
Omid Noorshams <o.noorshams@gmail.com>
2023-05-22 17:01:22 -0700

    add shade to flag

--- chapter-03--advanced-techniques/27-shaders/src/shaders/test/fragment.glsl
+++ chapter-03--advanced-techniques/27-shaders/src/shaders/test/fragment.glsl
@@ -1,12 +1,16 @@
 precision mediump float;
+
 uniform float time;
 uniform vec3 uColor;
 uniform sampler2D uTexture;
+
 varying float vRandom;
 varying vec2 vUv;
+varying float vElevation;
 
 void main(){
     // gl_FragColor = vec4(vRandom * vRandom, vRandom *vRandom *vRandom, vRandom, sin(vRandom));
     vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation*2.0+1.0;
     gl_FragColor = textureColor;
 }
