7ef5b132463ba7afb140006a6b9bb4d3b0080c33
Omid Noorshams <o.noorshams@gmail.com>
2023-05-24 00:32:37 -0700

    animate objects with useFrame

--- chapter-07--react-three-fiber/42-first-r3f-application/src/Experience.jsx
+++ chapter-07--react-three-fiber/42-first-r3f-application/src/Experience.jsx
@@ -1,9 +1,27 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 const Experience = () => {
+  const cubeRef = useRef();
+  useFrame(() => {
+    cubeRef.current.rotation.x += 0.01;
+    cubeRef.current.rotation.y += 0.01;
+    cubeRef.current.rotation.z += 0.01;
+  });
+
   return (
     <>
-      <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
+      <mesh position-x={2} ref={cubeRef}>
+        <boxGeometry />
+        <meshBasicMaterial color={"mediumpurple"} />
+      </mesh>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshBasicMaterial color={"orange"} />
+      </mesh>
+      <mesh position={[0, -1, 0]} rotation-x={-Math.PI * 0.5}>
+        <planeGeometry args={[10, 5]} />
+        <meshBasicMaterial color={"blue"} />
       </mesh>
     </>
   );
