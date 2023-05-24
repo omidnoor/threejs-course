f26d676ce8e67a52813c78adf29d5633d1860d13
Omid Noorshams <o.noorshams@gmail.com>
2023-05-23 18:46:47 -0700

    add color to particles

--- chapter-04--Shaders/30-animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ chapter-04--Shaders/30-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -2,6 +2,8 @@ uniform float uSize;
 
 attribute float aScale;
 
+varying vec3 vColor;
+
 void main(){
         vec4 modelPosition = modelMatrix * vec4(position, 1.0);
         vec4 viewPosition = viewMatrix * modelPosition;
@@ -10,4 +12,9 @@ void main(){
         gl_Position = projectionPosition;
         gl_PointSize = uSize  * aScale;
         gl_PointSize *= ( 1.0 / - viewPosition.z );
+
+        /**
+        * Color
+        */
+        vColor = color;
     }
