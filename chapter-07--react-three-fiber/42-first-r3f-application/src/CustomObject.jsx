3f6cea12732fb04e3cbd54a554f48cf3efe508d6
Omid Noorshams <o.noorshams@gmail.com>
2023-05-24 02:12:54 -0700

    correct normals of custom geometry with computeVertexNormals

--- chapter-07--react-three-fiber/42-first-r3f-application/src/CustomObject.jsx
+++ chapter-07--react-three-fiber/42-first-r3f-application/src/CustomObject.jsx
@@ -1,9 +1,17 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { BufferGeometry } from "three";
 import * as THREE from "three";
 
 const CustomObject = () => {
+  const geometryRef = useRef();
+
+  useEffect(() => {
+    geometryRef.current.computeVertexNormals();
+  }, []);
+
   const verticesCount = 10 * 3;
+
+  console.log(geometryRef);
 
   const positions = useMemo(() => {
     const positions = new Float32Array(verticesCount * 3);
@@ -17,7 +25,7 @@ const CustomObject = () => {
   return (
     <>
       <mesh>
-        <bufferGeometry>
+        <bufferGeometry ref={geometryRef}>
           <bufferAttribute
             attach="attributes-position"
             count={verticesCount}
@@ -25,7 +33,7 @@ const CustomObject = () => {
             array={positions}
           />
         </bufferGeometry>
-        <meshBasicMaterial color="red" side={THREE.DoubleSide} />
+        <meshStandardMaterial color="red" side={THREE.DoubleSide} />
       </mesh>
     </>
   );
