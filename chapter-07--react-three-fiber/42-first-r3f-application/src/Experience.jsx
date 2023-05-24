de076d88c4ba93d057621c8f8a9c8e733003d414
Omid Noorshams <o.noorshams@gmail.com>
2023-05-24 00:49:44 -0700

    get cameraand gl from useThree. use orbitControl with extend

--- chapter-07--react-three-fiber/42-first-r3f-application/src/Experience.jsx
+++ chapter-07--react-three-fiber/42-first-r3f-application/src/Experience.jsx
@@ -1,24 +1,33 @@
-import { useFrame } from "@react-three/fiber";
+import { useFrame, extend, useThree } from "@react-three/fiber";
 import { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+extend({ OrbitControls: OrbitControls });
 
 const Experience = () => {
   const cubeRef = useRef();
-  useFrame(() => {
-    cubeRef.current.rotation.x += 0.01;
-    cubeRef.current.rotation.y += 0.01;
-    cubeRef.current.rotation.z += 0.01;
-  });
+  const { camera, gl } = useThree();
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.z += delta;
+    cubeRef.current.rotation.x += delta;
+    cubeRef.current.rotation.y += delta;
+  }, []);
 
   return (
     <>
-      <mesh position-x={2} ref={cubeRef}>
-        <boxGeometry />
-        <meshBasicMaterial color={"mediumpurple"} />
-      </mesh>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color={"orange"} />
-      </mesh>
+      <orbitControls args={[camera, gl.domElement]} />
+
+      <group>
+        <mesh position-x={2} ref={cubeRef}>
+          <boxGeometry />
+          <meshBasicMaterial color={"mediumpurple"} />
+        </mesh>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color={"orange"} />
+        </mesh>
+      </group>
       <mesh position={[0, -1, 0]} rotation-x={-Math.PI * 0.5}>
         <planeGeometry args={[10, 5]} />
         <meshBasicMaterial color={"blue"} />
