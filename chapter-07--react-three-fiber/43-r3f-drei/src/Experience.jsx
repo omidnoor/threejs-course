b22381ab4f921ccdcc529d8a0801b53c3c77212e
Omid Noorshams <o.noorshams@gmail.com>
2023-05-24 18:33:06 -0700

    add text using Text from Deri

--- chapter-07--react-three-fiber/43-r3f-drei/src/Experience.jsx
+++ chapter-07--react-three-fiber/43-r3f-drei/src/Experience.jsx
@@ -3,6 +3,7 @@ import {
   TransformControls,
   PivotControls,
   Html,
+  Text,
 } from "@react-three/drei";
 import { useRef } from "react";
 
@@ -12,10 +13,8 @@ export default function Experience() {
   return (
     <>
       <OrbitControls makeDefault />
-
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
-
       <PivotControls
         anchor={[0, 0, 0]}
         depthTest={false}
@@ -37,17 +36,25 @@ export default function Experience() {
           </Html>
         </mesh>
       </PivotControls>
-
       <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
       {/* <TransformControls object={cube} mode="translate" /> */}
-
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="green" />
       </mesh>
+      <Text
+        font="./bangers-v20-latin-regular.woff"
+        fontSize={1}
+        position={[0, 2, -1]}
+        maxWidth={2}
+        textAlign="center"
+        color="salmon"
+      >
+        I love R3F
+      </Text>
     </>
   );
 }
