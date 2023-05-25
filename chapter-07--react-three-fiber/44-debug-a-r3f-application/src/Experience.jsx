9257dbc9238f7b3b149458b282e9bd272a06bcbe
Omid Noorshams <o.noorshams@gmail.com>
2023-05-24 22:11:30 -0700

    add debuging gui and explore tweaks

--- chapter-07--react-three-fiber/44-debug-a-r3f-application/src/Experience.jsx
+++ chapter-07--react-three-fiber/44-debug-a-r3f-application/src/Experience.jsx
@@ -1,9 +1,39 @@
-import { useControls } from "leva";
+import { useControls, button } from "leva";
 import { OrbitControls } from "@react-three/drei";
 
 export default function Experience() {
-  const controls = useControls({
-    position: -2,
+  const { position, color, visible } = useControls("Sphere", {
+    position: {
+      value: {
+        x: -2,
+        y: 0,
+      },
+      min: -4,
+      max: 4,
+      step: 0.01,
+      joystick: "invertY",
+    },
+    color: {
+      value: "#ff0000",
+    },
+    visible: {
+      value: true,
+    },
+    clickMe: button(() => {
+      console.log("clicked");
+    }),
+    choice: {
+      options: ["a", "b", "c"],
+    },
+  });
+
+  const { scale } = useControls("Cube", {
+    scale: {
+      value: 1.5,
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+    },
   });
 
   return (
@@ -13,14 +43,14 @@ export default function Experience() {
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh position-x={-2}>
+      <mesh position={[position.x, position.y, 0]}>
         <sphereGeometry />
-        <meshStandardMaterial color="orange" />
+        <meshStandardMaterial color={color} />
       </mesh>
 
-      <mesh position-x={2} scale={1}>
+      <mesh position-x={2} scale={scale}>
         <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
+        <meshStandardMaterial color="mediumpurple" transparent={true} />
       </mesh>
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
