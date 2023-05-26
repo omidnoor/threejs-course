de30623a08e109b380fa8f6aba1eed29be99c824
Omid Noorshams <o.noorshams@gmail.com>
2023-05-25 22:52:08 -0700

    create 3D text with Text3D

--- chapter-07--react-three-fiber/47-3d-text-with-r3f/src/Experience.jsx
+++ chapter-07--react-three-fiber/47-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,32 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls, Text3D, Center } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <Perf position="top-left" />
+      <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
-
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+      <mesh scale={1.5}>
+        <meshNormalMaterial />
+      </mesh>
+      <Center>
+        <Text3D
+          font="./fonts/helvetiker_regular.typeface.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          I LOVE R3F !
+          <meshNormalMaterial />
+        </Text3D>
+      </Center>
     </>
+  );
 }
