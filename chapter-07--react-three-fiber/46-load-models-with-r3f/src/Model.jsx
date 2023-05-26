3ea0c6afb16acaf959c676e429edcc6a682d0cd5
Omid Noorshams <o.noorshams@gmail.com>
2023-05-25 18:04:51 -0700

    preload the model using useGLTF.preload

--- chapter-07--react-three-fiber/46-load-models-with-r3f/src/Model.jsx
+++ chapter-07--react-three-fiber/46-load-models-with-r3f/src/Model.jsx
@@ -4,7 +4,7 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { useGLTF } from "@react-three/drei";
 
 const Model = () => {
-  const model = useGLTF("./hamburger.glb");
+  const model = useGLTF("./hamburger-draco.glb");
 
   //   const model = useLoader(GLTFLoader, "./hamburger.glb");
 
@@ -19,4 +19,7 @@ const Model = () => {
   //   );
   return <primitive object={model.scene} scale={0.35} position-y={-1} />;
 };
+
+useGLTF.preload("./hamburger-draco.glb");
+
 export default Model;
