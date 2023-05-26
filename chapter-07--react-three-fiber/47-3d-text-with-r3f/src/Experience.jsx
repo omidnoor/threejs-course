7358bf0a45df7287a37d174721f7e9ba6ae9f4ab
Omid Noorshams <o.noorshams@gmail.com>
2023-05-25 23:34:07 -0700

    proper way to create geometries in a loop: define one geometry and use ref inside that to setGeometry and use geometry in mesh inside the loop

--- chapter-07--react-three-fiber/47-3d-text-with-r3f/src/Experience.jsx
+++ chapter-07--react-three-fiber/47-3d-text-with-r3f/src/Experience.jsx
@@ -5,8 +5,12 @@ import {
   useMatcapTexture,
 } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Experience() {
+  const [geometry, setGeometry] = useState(null);
+
   const [textMatcapTexture] = useMatcapTexture(
     "605352_E9CCC5_C7A8A3_A89291",
     256,
@@ -16,8 +20,6 @@ export default function Experience() {
     256,
   );
 
-  const tempArray = [...Array(100)];
-  tempArray.map((item, index) => {});
   return (
     <>
       <Perf position="top-left" />
@@ -40,11 +42,12 @@ export default function Experience() {
           <meshMatcapMaterial matcap={textMatcapTexture} />
         </Text3D>
       </Center>
-
+      <torusKnotGeometry ref={setGeometry} args={[1, 0.4, 64, 64]} />
       {[...Array(100)].map((_, key) => {
         return (
           <mesh
             key={key}
+            geometry={geometry}
             position={[
               (Math.random() - 0.5) * 10,
               (Math.random() - 0.5) * 10,
@@ -57,7 +60,6 @@ export default function Experience() {
               Math.random() * Math.PI,
             ]}
           >
-            <torusKnotGeometry args={[1, 0.4, 64, 64]} />
             <meshMatcapMaterial matcap={donutMatcapTexture} />
           </mesh>
         );
