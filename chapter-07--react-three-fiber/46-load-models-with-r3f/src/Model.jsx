03e5c5ae21b18bb7d878bd2822dd52ff05c89ae1
Omid Noorshams <o.noorshams@gmail.com>
2023-05-25 18:10:26 -0700

    Clone models

--- chapter-07--react-three-fiber/46-load-models-with-r3f/src/Model.jsx
+++ chapter-07--react-three-fiber/46-load-models-with-r3f/src/Model.jsx
@@ -1,7 +1,7 @@
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, Clone } from "@react-three/drei";
 
 const Model = () => {
   const model = useGLTF("./hamburger-draco.glb");
@@ -17,7 +17,14 @@ const Model = () => {
   //       loader.setDRACOLoader(dracoLoader);
   //     },
   //   );
-  return <primitive object={model.scene} scale={0.35} position-y={-1} />;
+  //   return <primitive object={model.scene} scale={0.35} position-y={-1} />;
+  return (
+    <>
+      <Clone object={model.scene} scale={0.2} position-y={-1} />;
+      <Clone object={model.scene} scale={0.2} position-x={4} />;
+      <Clone object={model.scene} scale={0.2} position-x={-2} />;
+    </>
+  );
 };
 
 useGLTF.preload("./hamburger-draco.glb");
