632ac1fe8e90817c10c35a02cd3cef8c4c15a57d
Omid Noorshams <o.noorshams@gmail.com>
2023-05-27 18:33:24 -0700

    create custom pass displacementPass: bee hive illusion

--- chapter-05--extra/32-post-processing/src/script.js
+++ chapter-05--extra/32-post-processing/src/script.js
@@ -269,9 +269,51 @@ const displacementShader = {
 };
 
 const displacementPass = new ShaderPass(displacementShader);
-displacementPass.enabled = true;
+displacementPass.enabled = false;
 displacementPass.material.uniforms.uTime.value = 0;
 effectComposer.addPass(displacementPass);
+
+gui.add(displacementPass, "enabled").name("displacement");
+
+// Displacement2 pass
+const displacement2Shader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    uNormalMap: { value: null },
+  },
+  vertexShader: `
+      varying vec2 vUv;
+        void main(){
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            vUv = uv;
+        }
+        `,
+  fragmentShader: `
+      uniform sampler2D tDiffuse; 
+      uniform sampler2D uNormalMap; 
+      varying vec2 vUv;
+      void main(){
+        vec3 normalColor = texture2D(uNormalMap, vUv).xyz * 2.0 - 1.0;
+         vec2 newUv = vUv + normalColor.xy * 0.1;
+         vec4 color = texture2D(tDiffuse, newUv);
+
+        vec3 lightDirection = normalize(vec3(-1.0, 1.0, 0.0));
+        float lightness = clamp(dot(normalColor, lightDirection), 0.0, 0.5);
+        color.rgb += lightness * 2.0;
+
+        gl_FragColor = color;
+      }
+      `,
+};
+
+const displacement2Pass = new ShaderPass(displacement2Shader);
+displacement2Pass.enabled = false;
+displacement2Pass.material.uniforms.uNormalMap.value = textureLoader.load(
+  "/textures/interfaceNormalMap.png",
+);
+effectComposer.addPass(displacement2Pass);
+
+gui.add(displacement2Pass, "enabled").name("BeeHive");
 
 // Gamma correction pass
 const gammaCorrectionShader = new ShaderPass(GammaCorrectionShader);
