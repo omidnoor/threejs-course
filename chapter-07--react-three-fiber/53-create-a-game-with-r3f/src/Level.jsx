71c24f0c8ccf9ddfe86fc695462c3482d652076d
Omid Noorshams <o.noorshams@gmail.com>
2023-05-29 02:52:05 -0700

    create materials outside of component and use them in mesh

--- chapter-07--react-three-fiber/53-create-a-game-with-r3f/src/Level.jsx
+++ chapter-07--react-three-fiber/53-create-a-game-with-r3f/src/Level.jsx
@@ -1,6 +1,12 @@
 import * as THREE from "three";
 
+THREE.ColorManagement.legacyMode = false;
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obsticleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
 
 function BlockStart({ position = [0, 0, 0] }) {
   return (
@@ -8,12 +14,11 @@ function BlockStart({ position = [0, 0, 0] }) {
       <group position={position}>
         <mesh
           geometry={boxGeometry}
+          material={floor1Material}
           position={[0, -0.1, 0]}
           scale={[4, 0.2, 4]}
           receiveShadow
-        >
-          <meshStandardMaterial color="limegreen" />
-        </mesh>
+        />
       </group>
     </>
   );
