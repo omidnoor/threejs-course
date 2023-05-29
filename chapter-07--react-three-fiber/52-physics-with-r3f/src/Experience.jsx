98e5428bfd293cfbdbcb57b8d8d8ca828f6ebabb
Omid Noorshams <o.noorshams@gmail.com>
2023-05-28 18:34:52 -0700

    applyImpulse and applyTorqueImpulse on objects. change gravity and gravityScale

--- chapter-07--react-three-fiber/52-physics-with-r3f/src/Experience.jsx
+++ chapter-07--react-three-fiber/52-physics-with-r3f/src/Experience.jsx
@@ -14,8 +14,31 @@ import {
   TrimeshCollider,
   HeightfieldCollider,
 } from "@react-three/rapier";
+import { useRef } from "react";
 
 export default function Experience() {
+  const cube = useRef();
+  const sphere = useRef();
+
+  const cubeJump = () => {
+    console.log(cube.current.applyImpulse);
+    cube.current.applyImpulse({ x: 0, y: 5, z: 0 });
+    cube.current.applyTorqueImpulse({
+      x: Math.random() - 0.5,
+      y: Math.random() - 0.5,
+      z: Math.random() - 0.5,
+    });
+  };
+
+  const sphereJump = () => {
+    sphere.current.applyImpulse({ x: 0, y: 5, z: 0 });
+    sphere.current.applyTorqueImpulse({
+      x: Math.random() - 0.5,
+      y: Math.random() - 0.5,
+      z: Math.random() - 0.5,
+    });
+  };
+
   return (
     <>
       <Perf position="top-left" />
@@ -25,21 +48,21 @@ export default function Experience() {
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <Physics>
+      <Physics gravity={[0, -3.5, 0]}>
         <Debug />
-        <RigidBody colliders="ball">
-          <mesh castShadow position={[0, 4, 0]}>
+        <RigidBody colliders="ball" gravityScale={0.1} ref={sphere}>
+          <mesh castShadow position={[-1.5, 2, 0]} onClick={sphereJump}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        {/* <RigidBody>
-          <mesh castShadow position={[2, 2, 0]} scale={2}>
+        <RigidBody ref={cube}>
+          <mesh castShadow position={[1.5, 2, 0]} scale={1} onClick={cubeJump}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
-        </RigidBody> */}
+        </RigidBody>
 
         {/* <RigidBody colliders="hull">
           <mesh rotation-x={Math.PI * 0.5 * 0.1} position={[1, -0.5, 0]}>
@@ -53,27 +76,27 @@ export default function Experience() {
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
         </RigidBody> */}
-        <RigidBody
+        {/* <RigidBody
           colliders={false}
           rotation={[Math.PI * 0.5, 0, 0]}
           position={[0, 1, 0]}
-        >
-          {/* <CuboidCollider args={[1.5, 1.5, 0.5]} />
+        > */}
+        {/* <CuboidCollider args={[1.5, 1.5, 0.5]} />
           <CuboidCollider
             args={[0.25, 1, 0.25]}
             position={[0, 0, 1]}
             rotation={[Math.PI * 0.5 * 0.1, 0, 0]}
           /> */}
-          <BallCollider
+        {/* <BallCollider
             args={[1.5]}
             // position={[0, 0, 1]}
             // rotation={[Math.PI * 0.5 * 0.1, 0, 0]}
-          />
-          <mesh>
+          /> */}
+        {/* <mesh>
             <torusGeometry args={[1, 0.5, 16, 32]} />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
-        </RigidBody>
+        </RigidBody> */}
 
         <RigidBody type="fixed">
           <mesh receiveShadow position-y={-1.25}>
