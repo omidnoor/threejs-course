a1a849c3c7e76aa38b061a2154d778e9a06bfb39
Omid Noorshams <o.noorshams@gmail.com>
2023-05-29 03:39:23 -0700

    setNextKinematicRotation for limbo obstacle animation

--- chapter-07--react-three-fiber/53-create-a-game-with-r3f/src/Level.jsx
+++ chapter-07--react-three-fiber/53-create-a-game-with-r3f/src/Level.jsx
@@ -30,7 +30,7 @@ function BlockStart({ position = [0, 0, 0] }) {
 function BlockSpinner({ position = [0, 0, 0] }) {
   const obstacle = useRef(null);
   const [speed, setSpeed] = useState(() => {
-    return Math.random() * 0.6 + 0.4;
+    return (Math.random() + 0.4) * (Math.random() < 0.5 ? -1 : 1);
   });
 
   useFrame((state) => {
@@ -38,6 +38,53 @@ function BlockSpinner({ position = [0, 0, 0] }) {
     const rotation = new THREE.Quaternion();
     rotation.setFromEuler(new THREE.Euler(0, time * speed, 0));
     obstacle.current.setNextKinematicRotation(rotation);
+  });
+
+  return (
+    <>
+      <group position={position}>
+        <mesh
+          geometry={boxGeometry}
+          material={floor2Material}
+          position={[0, -0.1, 0]}
+          scale={[4, 0.2, 4]}
+          castShadow
+          receiveShadow
+        />
+        <RigidBody
+          type="kinematicPosition"
+          restitution={0.2}
+          friction={0}
+          ref={obstacle}
+        >
+          <mesh
+            geometry={boxGeometry}
+            material={obstacleMaterial}
+            scale={[3.5, 0.4, 0.3]}
+            position={[0, 0.2, 0]}
+            castShadow
+            receiveShadow
+          />
+        </RigidBody>
+      </group>
+    </>
+  );
+}
+
+function BlockLimbo({ position = [0, 0, 0] }) {
+  const obstacle = useRef(null);
+  const [timeOffset, setTimeOffset] = useState(() => {
+    return Math.random() * Math.PI * 2;
+  });
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const y = Math.sin(time + timeOffset) + 1.2;
+    obstacle.current.setNextKinematicTranslation({
+      x: position[0],
+      y: y,
+      z: position[2],
+    });
   });
 
   return (
@@ -73,8 +120,9 @@ function BlockSpinner({ position = [0, 0, 0] }) {
 const Level = () => {
   return (
     <>
-      <BlockStart position={[0, 0, 4]} />
-      <BlockSpinner position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 12]} />
+      <BlockSpinner position={[0, 0, 8]} />
+      <BlockLimbo position={[0, 0, 4]} />
     </>
   );
 };
