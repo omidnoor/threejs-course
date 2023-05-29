dd02bcac73c4b493d9f1fb1adc1796befbe43d0d
Omid Noorshams <o.noorshams@gmail.com>
2023-05-29 02:36:15 -0700

    create BlockStart

--- chapter-07--react-three-fiber/53-create-a-game-with-r3f/src/Experience.jsx
+++ chapter-07--react-three-fiber/53-create-a-game-with-r3f/src/Experience.jsx
@@ -1,15 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
 import Lights from "./Lights.jsx";
 import Level from "./Level.jsx";
+import { Physics, Debug } from "@react-three/rapier";
 
 export default function Experience() {
   return (
     <>
       <OrbitControls makeDefault />
+      <Physics>
+        <Lights />
 
-      <Lights />
-
-      <Level />
+        <Level />
+      </Physics>
     </>
   );
 }
