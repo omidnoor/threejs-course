d5d4ffa91ad754ab3b4504f34ba019730ec6d631
Omid Noorshams <o.noorshams@gmail.com>
2023-05-28 17:24:46 -0700

    add RigidBody and Physics from Rapier@0.9

--- chapter-07--react-three-fiber/52-physics-with-r3f/src/Experience.jsx
+++ chapter-07--react-three-fiber/52-physics-with-r3f/src/Experience.jsx
@@ -1,31 +1,40 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { Physics, RigidBody, Debug } from "@react-three/rapier";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <Perf position="top-left" />
+      <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
+      <Physics>
+        <Debug />
+        <RigidBody>
+          <mesh castShadow position={[-2, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
-        </mesh>
+          </mesh>
+        </RigidBody>
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
+        <RigidBody>
+          <mesh castShadow position={[2, 2, 0]} scale={2}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+          </mesh>
+        </RigidBody>
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+          </mesh>
+        </RigidBody>
+      </Physics>
     </>
+  );
 }
